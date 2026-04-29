8b5f99a2edf1e39b73f14758ebc853279c41df97
woori02 <jwl0819@naver.com>
2026-04-29 17:18:35 +0900

    [#44][FE] OAuth 콜백 쿠키 기반으로 수정

--- frontend/src/pages/AuthCallbackPage.jsx
+++ frontend/src/pages/AuthCallbackPage.jsx
@@ -5,19 +5,7 @@ export default function AuthCallbackPage() {
   const navigate = useNavigate()
 
   useEffect(() => {
-    const hash = window.location.hash.substring(1)
-    const params = new URLSearchParams(hash)
-
-    const accessToken = params.get('access_token')
-    const refreshToken = params.get('refresh_token')
-
-    if (accessToken) {
-      localStorage.setItem('access_token', accessToken)
-      if (refreshToken) localStorage.setItem('refresh_token', refreshToken)
-      navigate('/projects', { replace: true })
-    } else {
-      navigate('/login', { replace: true })
-    }
+    navigate('/projects', { replace: true })
   }, [])
 
   return (
