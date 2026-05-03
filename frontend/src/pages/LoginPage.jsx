84675c636cb146c13e8c1d4bb4119a642dde42f9
woori02 <jwl0819@naver.com>
2026-05-03 22:06:00 +0900

    [#62][FE] 로그인 페이지 수정(카카오 제거)

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,5 @@
 import styles from './LoginPage.module.css'
 import { SiGoogle, SiNaver } from 'react-icons/si'
-import { RiKakaoTalkFill } from 'react-icons/ri'
 
 
 export default function LoginPage() {
@@ -22,10 +21,6 @@ export default function LoginPage() {
         <h1 className={styles.title}>로그인</h1>
 
         <div className={styles.btnGroup}>
-          
-          <button className={`${styles.socialBtn} ${styles.kakao}`} onClick={() => handleLogin('kakao')}>
-            <RiKakaoTalkFill size={35} /> Kakao로 시작하기
-          </button>
           <button className={`${styles.socialBtn} ${styles.google}`} onClick={() => handleLogin('google')}>
             <img src="/google-logo.svg" width={27} height={27} /> Google로 시작하기
           </button>
